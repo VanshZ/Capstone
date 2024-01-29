516d92350a8dd68c3fc285d58212ee9152af82bc
Vansh Zota <68395796+VanshZ@users.noreply.github.com>
2024-01-29 15:52:16 -0500

    reformat home page - new look to the website

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -8,6 +8,7 @@ import { Separator } from '@/components/ui/separator';
 import { DataTable } from '@/components/ui/data-table';
 import { columns } from './components/columns';
 import { formatter } from '@/lib/utils';
+import backgroundImage from './path/to/your/image.jpg';
 
 const HomePage = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -51,11 +52,18 @@ const HomePage = () => {
     }
 
     return (
-        <Container>
-            <div className="pt-16">
-                <div className="flex justify-center items-center min-h-screen">
-                    <div className="w-full max-w-4xl px-4">
-                        <h1 className="text-center text-2xl font-bold mb-4">Property Search</h1>
+            <div style={{
+            backgroundImage: `url(https://i0.wp.com/www.sandiegorealestatephotography.com/wp-content/uploads/2023/11/DALL%C2%B7E-2023-11-06-14.45.19-A-stunning-high-resolution-wide-thumbnail-image-for-a-real-estate-photography-guide-video.-The-scene-shows-the-San-Diego-skyline-in-the-background-wit.png?fit=1792%2C1024&ssl=1)`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundAttachment: 'fixed',
+            minHeight: '100vh', // Ensure it covers at least the full height of the viewport
+        }}>
+            <Container>
+                <div className="pt-16">
+                    <div className="flex justify-center items-center max-h-screen" style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)', backdropFilter: 'blur(5px)' }}>
+                        <div className="w-full max-w-8xl px-4">
+                        <h1 className="text-center text-4xl font-bold mb-4">Search ROIPro</h1>
                         <input
                             type="text"
                             placeholder="Enter location"
@@ -79,11 +87,12 @@ const HomePage = () => {
                                 searchKey={searchQuery} 
                                 detailPageUrl="/property-details"
                             />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </Container>
+            </Container>
+        </div>
     );
 };
 
